8b27cf73f2c3e42e9a766128aac8c264bf9f7da0
Kevin Mann <kevinrmann@gmail.com>
2018-01-09 14:06:52 -0700

    Initial commit

--- SceneKitBlocks/ViewController.swift
+++ SceneKitBlocks/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import SceneKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    @IBOutlet weak var sceneView: SCNView!
+    var scene: SCNScene!
+    var floorNode: SCNNode!
+    var floor: SCNFloor!
+    var cameraNode: SCNNode!
+    var camera: SCNCamera!
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupScene()
+        setupFloor()
+        setupCamera()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Scene Methods
+    func spawnSactional() {
+        let geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+        let node = SCNNode(geometry: geometry)
+        node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        node.position = SCNVector3(0, 5, -15)
+        scene.rootNode.addChildNode(node)
     }
-
-
+    
+    func setupCamera() {
+        guard let cameraNode = scene.rootNode.childNode(withName: "camera", recursively: true), let camera = cameraNode.camera else {
+            return
+        }
+        self.cameraNode = cameraNode
+        self.camera = camera
+    }
+    
+    func setupFloor() {
+        guard let floorNode = scene.rootNode.childNode(withName: "floor", recursively: true), let floor = floorNode.geometry as? SCNFloor else {
+            return
+        }
+        floorNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        self.floor = floor
+        self.floorNode = floorNode
+    }
+    
+    func setupScene() {
+        guard let scene = SCNScene(named: "art.scnassets/scene.scn") else {
+            return
+        }
+        self.scene = scene
+        sceneView.scene = scene
+    }
+    
+    // MARK: - Actions
+    @IBAction func createBlock() {
+        spawnSactional()
+    }
 }
-
